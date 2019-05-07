ae40266adab5bfdf19834cd916ae1be18962e0ec
kwseeker <xiaohuileee@gmail.com>
2019-05-08 01:47:15 +0800

    temp

--- ns-core/src/test/java/top/kwseeker/core/guava/CacheTest.java
+++ ns-core/src/test/java/top/kwseeker/core/guava/CacheTest.java
@@ -6,18 +6,50 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.junit.Before;
 import org.junit.Test;
-import org.omg.CORBA.TIMEOUT;
 
+import javax.annotation.Nonnull;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 缓存
+ *
+ * LoadingCache API
+ *
+ * 1
+ * V apply(K key)
+ *
+ * Deprecated. Provided to satisfy the Function interface; use get(K) or getUnchecked(K) instead.
+ *
+ * 2
+ * ConcurrentMap<K,V> asMap()
+ *
+ * Returns a view of the entries stored in this cache as a thread-safe map.
+ *
+ * 3
+ * V get(K key)
+ *
+ * Returns the value associated with key in this cache, first loading that value if necessary.
+ *
+ * 4
+ * ImmutableMap<K,V> getAll(Iterable<? extends K> keys)
+ *
+ * Returns a map of the values associated with keys, creating or retrieving those values if necessary.
+ *
+ * 5
+ * V getUnchecked(K key)
+ *
+ * Returns the value associated with key in this cache, first loading that value if necessary.
+ *
+ * 6
+ * void refresh(K key)
+ *
+ * Loads a new value for key, possibly asynchronously.
  */
 public class CacheTest {
 
-    private Cache<Object, Object> cache;
-    private Cache<Object, Object> cache1;
+    private LoadingCache<Key, Value> cache;
+    private Cache<Key, Value> cache1;
 
     @Before
     public void init() {
@@ -30,14 +62,14 @@ public class CacheTest {
                 //.removalListener()
                 //.recordStats()
                 //.refreshAfterWrite()
-                .build(new CacheLoader<Object, Object>() {
-                    public Object load(Object key) throws Exception {   //如果缓存加载器抛出一个未经检查的异常，get（K）将抛出一个包含它的UncheckedExecutionException。
-                        return
+                .build(new CacheLoader<Key, Value>() {
+                    @Override
+                    public Value load(@Nonnull Key key) throws Exception {   //如果缓存加载器抛出一个未经检查的异常，get（K）将抛出一个包含它的UncheckedExecutionException。
+                        return getValueByKey(key);
                     }
-
-                    public Graph load(Key key) { // no checked exception
-                        return createExpensiveGraph(key);
-                    }
+                    //public Graph load(Key key) { // no checked exception
+                    //    return createExpensiveGraph(key);
+                    //}
                 });
 
         cache1 = CacheBuilder.newBuilder().recordStats().build();
@@ -46,29 +78,73 @@ public class CacheTest {
     @Test
     public void testPut() {
         //直接插入数据
-        cache.put(key, value)
-
-        cache1.put("one", 1);
+        //cache.put(key, value)
+        //
+        //cache1.put("one", 1);
     }
 
     @Test
     public void testGet() {
         try {
-            return cache.get(key);
+            Key key1 = new Key("1");
+            Key key2 = new Key("2");
+            long beginTime = System.currentTimeMillis();
+            //第一次通过load获取
+            System.out.println(cache.get(key1));
+            System.out.println(cache.get(key2));
+            System.out.println(System.currentTimeMillis() - beginTime);
+            //第二次从缓存中直接获取
+            System.out.println(cache.get(key1));
+            System.out.println(cache.get(key2));
+            System.out.println(System.currentTimeMillis() - beginTime);
 
-            graphs.getUnchecked(key);
-
-            cache.get(key, new Callable<Value>() {
-                @Override
-                public Value call() throws AnyException {
-                    return doThingsTheHardWay(key);
-                }
-            });
+            //graphs.getUnchecked(key);
+            //
+            //cache.get(key, new Callable<Value>() {
+            //    @Override
+            //    public Value call() throws AnyException {
+            //        return doThingsTheHardWay(key);
+            //    }
+            //});
 
         } catch (ExecutionException e) {
-            throw new OtherException(e.getCause());
+            e.printStackTrace();
         }
     }
 
+    //实际应用中常常是从数据库或文件中读取
+    private static Value getValueByKey(Key key) {
+        try {
+            Thread.sleep(50);   //模拟缓存之前的耗时的读取过程
+            return new Value(Integer.valueOf(key.getKey()));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    //实际应用中键和值数据结构会比较复杂
+    public static class Key {
+        String key;
+
+        Key(String key) {
+            this.key = key;
+        }
+
+        String getKey() {
+            return key;
+        }
+    }
+
+    public static class Value {
+        Integer value;
+
+        Value(Integer value) {
+            this.value = value;
+        }
+
+        Integer getValue() {
+            return value;
+        }
+    }
 }
